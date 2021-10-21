c5d72bf76a3ae703069a04af0ea3720ad64ca390
quirkee.admin <quirkee.group@gmail.com>
2021-10-21 20:38:30 +0800

    fix provisioner

--- provisioner.tf
+++ provisioner.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "remove_and_upload_to_s3" {
   provisioner "local-exec" {
-    command = "aws s3 sync ${path.module}/../../../build s3://${aws_s3_bucket.website.id}"
+    command = "aws s3 sync ${path.module}/../../../build s3://${aws_s3_bucket.b.id}"
   }
 }
