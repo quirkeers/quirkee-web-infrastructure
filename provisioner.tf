637bb8f3709d57e24cbda81043bea6951c9b244c
quirkee.admin <quirkee.group@gmail.com>
2021-10-22 17:24:30 +0800

    fix

--- provisioner.tf
+++ provisioner.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "remove_and_upload_to_s3" {
+resource "aws_s3_bucket" "bucket_upload" {
   provisioner "local-exec" {
     command = "aws s3 sync ${path.module}/../../../build s3://${aws_s3_bucket.b.id}"
   }
