596c8d75d134b5fb982f5a423faef6de5440d44f
quirkee.admin <quirkee.group@gmail.com>
2021-10-22 10:30:17 +0800

    fix blocker

--- cdn.tf
+++ cdn.tf
@@ -2,6 +2,13 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.b.website_endpoint
     origin_id   = local.s3_origin_id
+    
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
 
   enabled             = true
@@ -40,12 +47,5 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     acm_certificate_arn = module.acm.acm_certificate_arn
     ssl_support_method = "sni-only"
-  }
-
-  custom_origin_config {
-    http_port              = "80"
-    https_port             = "443"
-    origin_protocol_policy = "http-only"
-    origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
   }
 }
