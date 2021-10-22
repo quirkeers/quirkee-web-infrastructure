48621bc91e2abb346668abe31d2484a17f09d150
quirkee.admin <quirkee.group@gmail.com>
2021-10-22 19:54:16 +0800

    modify provisioner

--- provisioner.tf
+++ provisioner.tf
@@ -5,3 +5,19 @@ resource "aws_s3_bucket_object" "test" {
   source = "./${path.module}/../../../build/${each.value}"
   etag = filemd5("./${path.module}/../../../build/${each.value}")
 }
+
+resource "aws_s3_bucket_object" "test" {
+  for_each = fileset("${path.module}/../../../build/static/css", "**")
+  bucket = aws_s3_bucket.b.id
+  key = "static/css/${each.value}"
+  source = "./${path.module}/../../../build/static/css/${each.value}"
+  etag = filemd5("./${path.module}/../../../build/static/css/${each.value}")
+}
+
+resource "aws_s3_bucket_object" "test" {
+  for_each = fileset("${path.module}/../../../build/static/js", "**")
+  bucket = aws_s3_bucket.b.id
+  key = "static/js/${each.value}"
+  source = "./${path.module}/../../../build/static/js/${each.value}"
+  etag = filemd5("./${path.module}/../../../build/static/js/${each.value}")
+}
