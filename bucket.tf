ac6c35e7f478899d61c37d3eed68cdafc14ff376
quirkee.admin <quirkee.group@gmail.com>
2021-11-12 23:54:41 +0800

    revert permission

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,19 @@
 resource "aws_s3_bucket" "b" {
   bucket = local.website_bucket_name
   acl    = "private"
+  policy = <<EOF
+{
+  "Version":"2012-10-17",
+  "Statement":[{
+        "Sid":"PublicReadForGetBucketObjects",
+        "Effect":"Allow",
+          "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${local.website_bucket_name}/*"]
+    }
+  ]
+}
+EOF
 
   force_destroy = true
 
